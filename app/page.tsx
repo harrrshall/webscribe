31948c6c08f679f3a83e6215a7d3bf8110c98258
CyberNovas <92268499+harrrshall@users.noreply.github.com>
2024-08-28 16:36:11 +0530

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Mainpage } from "@/components/mainpage";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background text-foreground">
       <Mainpage />
+      <Analytics />
     </main>
   );
 }
